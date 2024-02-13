1284c65574108767fc1ba5984f700e2d246390b1
Joaquin Thiogo <92239155+smthlikeyou24@users.noreply.github.com>
2024-02-13 16:10:08 +0700

    add cors

--- src/main/java/joaquinthiogo/inventorymanagementapi/WebConfiguration.java
+++ src/main/java/joaquinthiogo/inventorymanagementapi/WebConfiguration.java
@@ -4,6 +4,7 @@ import joaquinthiogo.inventorymanagementapi.resolver.UserArgumentResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,5 +18,10 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(userArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/*").allowedOrigins("*");
     }
 }
